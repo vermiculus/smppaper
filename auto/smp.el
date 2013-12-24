fb93152b7bf7b3108eea0a9e190795088d6f1784
Sean Allred <allred.sean@gmail.com>
2013-12-24 00:21:18 -0500

    Reference management

--- auto/smp.el
+++ auto/smp.el
@@ -1,10 +1,16 @@
 (TeX-add-style-hook "smp"
  (lambda ()
+    (LaTeX-add-bibliographies
+     "refs")
     (TeX-run-style-hooks
+     "mwe"
      "hyperref"
+     "biblatex"
+     "autocite=footnote"
      "latex2e"
      "cs-smp10"
      "cs-smp"
      ""
-     "abstract")))
+     "abstract"
+     "sections/introduction")))
 
