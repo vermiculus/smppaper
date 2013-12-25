bbb53cfd18a7c5ba95d880465bf1d723a791ff76
Sean Allred <allred.sean@gmail.com>
2013-12-25 01:32:40 -0500

    Class-level keys (again?), outline, mark-up

--- auto/cs-smp.el
+++ auto/cs-smp.el
@@ -3,12 +3,11 @@
     (TeX-add-symbols
      "Dash")
     (TeX-run-style-hooks
-     "geometry"
-     "margin=1in"
      "xparse"
      "log-declarations=false"
      "art10"
      "article"
+     "l3keys2e"
      "expl3"
      "")))
 
---
+++ auto/interface-graph-builder.el
@@ -0,0 +1,5 @@
+(TeX-add-style-hook "interface-graph-builder"
+ (lambda ()
+    (LaTeX-add-labels
+     "sec:graph-builder")))
+
