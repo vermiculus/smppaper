ebec1aadd102574f73c506816172e721a9848654
Sean Allred <allred.sean@gmail.com>
2014-03-01 18:57:54 -0500

    Add import/export tables

--- auto/interface-interop.el
+++ auto/interface-interop.el
@@ -2,6 +2,11 @@
  (lambda ()
     (LaTeX-add-labels
      "sec:iface-interoperability"
+     "tab:interop:import"
+     "tab:interop:export"
      "sec:interop:import"
-     "sec:interop:export")))
+     "sec:interop:export")
+    (TeX-add-symbols
+     "yes"
+     "no")))
 
