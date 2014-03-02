cf60185760a3109ece73fb9cca50b066dae086cd
Sean Allred <allred.sean@gmail.com>
2014-03-02 00:39:15 -0500

    Miscellaneous changes

--- auto/interface-interop.el
+++ auto/interface-interop.el
@@ -2,9 +2,12 @@
  (lambda ()
     (LaTeX-add-labels
      "sec:iface-interoperability"
+     "tab:format-spec"
      "tab:interop:import"
      "tab:interop:export"
      "sec:interop:import"
+     "fig:iface:alg-test-import"
+     "fig:iface:alg-test-export"
      "sec:interop:export")
     (TeX-add-symbols
      "yes"
--- auto/interface-test.el
+++ auto/interface-test.el
@@ -3,8 +3,6 @@
     (LaTeX-add-labels
      "sec:interface-testing"
      "sec:interface-testing:import"
-     "fig:iface:alg-test-import"
-     "fig:iface:alg-test-export"
      "fig:iface:alg-test-controlled"
      "fig:iface:alg-test-batch")))
 
